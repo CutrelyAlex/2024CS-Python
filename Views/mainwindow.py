1321c805bcbf6d517d1038c4b93da465ed5f3d1b
song0910song <1409024971@qq.com>
2024-11-27 00:22:19 +0800

    基本模板-首页

---
+++ Views/mainwindow.py
@@ -0,0 +1,32 @@
+# This Python file uses the following encoding: utf-8
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowTitle("test")
+
+    # 关闭按钮的提示
+    def closeEvent(self, event):
+        r = QMessageBox.question(self, "提示：", "确定要关闭吗？", QMessageBox.Yes | QMessageBox.No)
+        if r == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
