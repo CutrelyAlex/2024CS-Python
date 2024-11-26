87648662d23d19fa702b46d70ac8ee9489ad5f58
song0910song <1409024971@qq.com>
2024-11-27 00:17:52 +0800

    ui文件

--- Debug.py
+++ Debug.py
@@ -5,9 +5,7 @@ dish_path = "dishes.json"
 # 测试用入口
 def test_main():
     dishes = convert_to_dishes(load_dishes_from_json(dish_path))
-    print(dishes[0].name)
-
-
+    print(dishes[0].display_info())
 
     
 if __name__ == "__main__":
