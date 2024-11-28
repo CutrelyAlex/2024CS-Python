fffb8f6210c519a5024dc71de68282e900bc842a
song0910song <1409024971@qq.com>
2024-11-28 20:46:08 +0800

    暂定UI

--- Views/mainwindow.py
+++ Views/mainwindow.py
@@ -1,12 +1,6 @@
 # This Python file uses the following encoding: utf-8
 import sys
-
 from PySide6.QtWidgets import QApplication, QMainWindow,QMessageBox
-
-# Important:
-# You need to run the following command to generate the ui_form.py file
-#     pyside6-uic form.ui -o ui_form.py, or
-#     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -14,7 +8,7 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setWindowTitle("test")
+        self.setWindowTitle(u"SZTU就餐管理系统")
 
     # 关闭按钮的提示
     def closeEvent(self, event):
