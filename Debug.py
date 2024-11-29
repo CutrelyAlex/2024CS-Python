c147b54a924142e78d78451b9e422afc017ac451
CutrelyAlex <huangqiongtong@gmail.com>
2024-11-29 20:19:50 +0800

    修改若干Bug，添加必要功能

--- Debug.py
+++ Debug.py
@@ -11,8 +11,7 @@ student = student_controller.find_student_by_name("陈烨烨")[0]
 
 print(student_controller.get_all_students()[0].name)
 
-d = student_controller.get_all_students()[0].dining_info_list[0].dishes[0]
-print(dish_controller.find_dish_by_name(d)[0].price)
+print(dish_controller.find_dish_by_name(student_controller.get_all_students()[0].dining_info_list[0].dishes[0])[0].price)
 
 
 
