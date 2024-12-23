66a74127772d5cd311ae5d89f6c59152e8e055de
song0910song <1409024971@qq.com>
2024-12-24 07:43:21 +0800

    修改.bat文件

--- 点此运行.bat
+++ 点此运行.bat
@@ -1,21 +1,23 @@
 @echo off
-cls
 
 if not exist .venv\Scripts\activate.bat (
+    echo createing a python venv
 	python -m venv .venv
 	)
 
 call .venv\Scripts\activate.bat
-
-cls
+echo finished a python venv
 
 if not exist requirements.txt (
+    echo not found the requirements.txt, please found the requirements and again it
     pause
     exit /b
 )
 
-
+echo installing python packages
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+
+echo run python flask app
 start http://127.0.0.1:5000
 python app.py
 
